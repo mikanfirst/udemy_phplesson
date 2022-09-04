7f3e16544fd6718b5002ad20ed4c662f8163d961
masaaki_komatsubara <78458742+mikanfirst@users.noreply.github.com>
2022-09-04 16:56:50 +0900

    ファイルの書き込み

---
+++ sample15.php
@@ -0,0 +1,12 @@
+<!-- ファイルへの書き込み -->
+<?php
+$success = file_put_contents('data/news.txt',
+    'ホームページをリニューアルしました');
+
+if ($success !== false) {
+    echo 'ファイルの書き込みが完了しました';
+} else {
+    echo 'ファイルの書き込みが失敗しました';
+}
+?>
+
